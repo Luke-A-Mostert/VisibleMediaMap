40b92bceb97ffa5d8d26faf4bf4910128cd28a54
Luke-Mostert <luke.m@digipen.edu>
2024-07-24 12:43:50 -0700

    seperated into panels

--- js/map.js
+++ js/map.js
@@ -2,6 +2,9 @@ var map;
 var markers = [];
 var keyContent;
 var popupOpen = false;
+var panelData = {};
+var selectedPanel = null;
+
 // Function to hide overlay
 function hideOverlay() {
   var overlay = document.querySelector(".overlay");
@@ -35,9 +38,9 @@ async function initMap() {
 
   var clusterer = new MarkerClusterer(map, [], {
     minimumClusterSize: 2,
-    gridSize: 20,
+    gridSize: 10,
     averageCenter: true,
-    maxZoom: 12,
+    maxZoom: 11,
     styles: [
       {
         url: "Data/cluster.png", // URL of the cluster icon image
@@ -97,7 +100,7 @@ async function initMap() {
   }
 
   // Function to create and show the popup
-  function showPopup(info) {
+  function showPopup(info, Lat, Long) {
     // Show overlay to block interactions with the map
     popupOpen = true;
     showOverlay();
@@ -117,6 +120,18 @@ async function initMap() {
     // Set the position of the popup
     popup.style.left = map.getDiv().offsetWidth / 3 + "px";
     popup.style.top = map.getDiv().offsetHeight / 9 + "px";
+
+    var miniMap = new google.maps.Map(document.getElementById("miniMap"), {
+      center: { lat: parseFloat(Lat), lng: parseFloat(Long) },
+      zoom: 15,
+      disableDefaultUI: true,
+    });
+
+    var miniMarker = new google.maps.Marker({
+      position: { lat: parseFloat(Lat), lng: parseFloat(Long) },
+      map: miniMap,
+    });
+    console.log("Mini map should be visible now."); // Debugging line
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////
@@ -138,12 +153,10 @@ async function initMap() {
     Lat,
     Long,
     Traffic,
-    Location,
+    Loc,
     BoardSize,
-    ImageFront,
-    ImageBack,
-    FacingFront,
-    FacingBack,
+    Img,
+    Facing,
     Pricing,
     Illuminated,
     Description
@@ -164,24 +177,26 @@ async function initMap() {
       Lat: Lat,
       Long: Long,
       Traffic: Traffic,
-      Location: Location,
+      Loc: Loc,
       BoardSize: BoardSize,
-      ImageFront: ImageFront,
-      ImageBack: ImageBack,
-      FacingFront: FacingFront,
-      FacingBack: FacingBack,
+      Img: Img,
+      Facing: Facing,
       Pricing: Pricing,
       Illuminated: Illuminated,
       Description: Description,
     });
 
-    marker.addListener("click", function () {
-      showPopup(contentString);
+    //marker.addListener("click", function () {
+    //  showPopup(contentString, Lat, Long);
+    //});
+
+    marker.addListener("click", () => {
+      openPanelSelector(name);
     });
 
     clusterer.addMarker(marker);
     markers.push(marker); // Store the marker in the markers array
-    createKey(name, Location);
+    createKey(name, Loc);
   }
   /////////////////////////////////////////////////////////////////////////////////////////
 
@@ -217,92 +232,54 @@ async function initMap() {
           values.push(currentValue.trim());
 
           // If the number of values is correct
-          if (values.length === 13) {
-            [
+          if (values.length === 11) {
+            let [
               ID,
               Lat,
               Long,
               Traffic,
-              Location,
+              Loc,
               BoardSize,
-              ImageFront,
-              ImageBack,
-              FacingFront,
-              FacingBack,
+              Img,
+              Facing,
               Pricing,
               Illuminated,
               Description,
             ] = values;
-            if (Lat && Long) {
-              var contentString = `
-                            <button class="close-button" onclick="closePopup()">X</button>
-                            <div class="popup-content">
-                                <h1>${Location}</h1>
-                                <div class = "images-container">
-                                    <div class = "image-item">
-                                    <img src="Data/${ImageFront}" alt="Billboard Image Front">
-                                    <p><strong>${FacingFront} Face</p>
-                                    </div>
-                                    <div class = "image-item">
-                                    <img src="Data/${ImageBack}" alt="Billboard Image Back">
-                                    <p><strong>${FacingBack} Face</p>
-                                    </div>
-                                </div>
-                                <div class="description-box">
-                                    <p>${Description}</p>
-                                </div>
-                                <div class="grid-container">
-                                    <div class="grid-item">
-                                        <h2 class="grid-item"><strong>Board Number</strong></h2>
-                                        <p>${ID}</p>
-                                    </div>
-                                    <div class="grid-item">
-                                        <h2 class="grid-item"><strong>Traffic</strong></h2>
-                                        <p> ${Traffic}</p>
-                                    </div>
-                                    <div class="grid-item">
-                                        <h2 class="grid-item"><strong>Board Size</strong></h2>
-                                        <p> ${BoardSize}</p>
-                                    </div>
-                                    <div class="grid-item">
-                                        <h2 class="grid-item"><strong>Lat/Long</strong></h2>
-                                        <p> <a href="https://www.google.com/maps?q=${Lat},${Long}" target="_blank">${
-                Lat + ", " + Long
-              }</a></p>
-                                    </div>
-                                    <div class="grid-item">
-                                        <h2 class="grid-item"><strong>Pricing</strong></h2>
-                                        <p>${Pricing}</p>
-                                    </div>
-                                    <div class="grid-item">
-                                        <h2 class="grid-item"><strong>Illuminated</strong></h2>
-                                        <p> ${Illuminated}</p>
-                                    </div>
-                                    
-                                </div>
-                                
-                            </div>
-                    `;
+            const boardId = ID.slice(0, -1);
+            const panelId = ID.slice(-1);
 
-              addMarker(
-                keyContent,
-                { lat: parseFloat(Lat), lng: parseFloat(Long) },
-                ID,
-                contentString,
-                Lat,
-                Long,
-                Traffic,
-                Location,
-                BoardSize,
-                ImageFront,
-                ImageBack,
-                FacingFront,
-                FacingBack,
-                Pricing,
-                Illuminated,
-                Description
-              );
+            if (!panelData[boardId]) {
+              panelData[boardId] = { Lat, Long, panels: {} };
             }
+            panelData[boardId].panels[panelId] = {
+              ID,
+              Lat,
+              Long,
+              Traffic,
+              Loc,
+              BoardSize,
+              Img,
+              Facing,
+              Pricing,
+              Illuminated,
+              Description,
+            };
+
+            if (!panelData[boardId]) panelData[boardId] = {};
+            panelData[boardId][panelId] = {
+              ID,
+              Lat,
+              Long,
+              Traffic,
+              Loc,
+              BoardSize,
+              Img,
+              Facing,
+              Pricing,
+              Illuminated,
+              Description,
+            };
           } else {
             console.error(
               `Error parsing CSV line ${index + 1}: Incorrect number of values`
@@ -310,10 +287,149 @@ async function initMap() {
           }
         }
       });
+
+      Object.keys(panelData).forEach((boardId) => {
+        const board = panelData[boardId];
+        const firstPanel =
+          board.panels["N"] ||
+          board.panels["S"] ||
+          board.panels["W"] ||
+          board.panels["E"];
+        const contentString = `
+          <button class="close-button" onclick="closePopup()">X</button>
+          <div class="popup-content">
+            <h1>${firstPanel.Loc}</h1>
+            <div class="images-container">
+              <div class="image-item">
+                <img src="Data/${firstPanel.Img}" alt="Billboard Image">
+                <p><strong>${firstPanel.Facing} Face</strong></p>
+              </div>
+              <div id="miniMap"></div>
+            </div>
+            <div class="description-box">
+              <p>${firstPanel.Description}</p>
+            </div>
+            <div class="grid-container">
+              <div class="grid-item"><strong>Board Number</strong><p>${firstPanel.ID}</p></div>
+              <div class="grid-item"><strong>Traffic</strong><p>${firstPanel.Traffic}</p></div>
+              <div class="grid-item"><strong>Board Size</strong><p>${firstPanel.BoardSize}</p></div>
+              <div class="grid-item"><strong>Lat/Long</strong><p><a href="https://www.google.com/maps?q=${firstPanel.Lat},${firstPanel.Long}" target="_blank">${firstPanel.Lat}, ${firstPanel.Long}</a></p></div>
+              <div class="grid-item"><strong>Pricing</strong><p>${firstPanel.Pricing}</p></div>
+              <div class="grid-item"><strong>Illuminated</strong><p>${firstPanel.Illuminated}</p></div>
+            </div>
+          </div>
+        `;
+        addMarker(
+          keyContent,
+          { lat: parseFloat(board.Lat), lng: parseFloat(board.Long) },
+          boardId,
+          contentString,
+          board.Lat,
+          board.Long,
+          firstPanel.Traffic,
+          firstPanel.Loc,
+          firstPanel.BoardSize,
+          firstPanel.Img,
+          firstPanel.Facing,
+          firstPanel.Pricing,
+          firstPanel.Illuminated,
+          firstPanel.Description
+        );
+      });
     })
     .catch((error) => {
       console.error("Error fetching the CSV file:", error);
     });
+
+  /////////////////////////////////////////////////////////////////////////////////////////
+
+  function openPanelSelector(markerId) {
+    const panelSelector = document.getElementById("panel-selector");
+    const panelDropdown = document.getElementById("panelDropdown");
+
+    if (panelDropdown) {
+      panelDropdown.innerHTML = "";
+
+      const board = panelData[markerId];
+      if (board && board.panels) {
+        Object.keys(board.panels).forEach((panelId) => {
+          const panel = board.panels[panelId];
+          const option = document.createElement("option");
+          option.value = panel.ID;
+          option.text = `${panel.Facing} - ${panel.Description}`;
+          panelDropdown.appendChild(option);
+        });
+      } else {
+        console.error(`No panels found for markerId: ${markerId}`);
+      }
+
+      if (panelSelector) {
+        panelSelector.classList.remove("hidden");
+      } else {
+        console.error("Panel selector element not found.");
+      }
+
+      const viewPanelBtn = document.getElementById("viewPanelBtn");
+      if (viewPanelBtn) {
+        viewPanelBtn.onclick = () => {
+          const selectedPanelId = panelDropdown.value;
+          showPanel(selectedPanelId);
+        };
+      } else {
+        console.error("View Panel button not found.");
+      }
+    } else {
+      console.error("Panel dropdown element not found.");
+    }
+  }
+
+  function showPanel(panelId) {
+    let panelFound = false; // Flag to check if panel is found
+
+    for (const boardId in panelData) {
+      const board = panelData[boardId];
+      if (board && board.panels) {
+        for (const panelIdKey in board.panels) {
+          const panel = board.panels[panelIdKey];
+          if (panel.ID === panelId) {
+            const contentString = `
+              <button class="close-button" onclick="closePopup()">X</button>
+              <div class="popup-content">
+                <h1>${panel.Loc}</h1>
+                <div class="images-container">
+                  <div class="image-item">
+                    <img src="Data/${panel.Img}" alt="Billboard Image Front">
+                    <p><strong>${panel.Facing} Face</strong></p>
+                  </div>
+                  <div id="miniMap"></div>
+                </div>
+                <div class="description-box">
+                  <p>${panel.Description}</p>
+                </div>
+                <div class="grid-container">
+                  <div class="grid-item"><strong>Board Number</strong><p>${panel.ID}</p></div>
+                  <div class="grid-item"><strong>Traffic</strong><p>${panel.Traffic}</p></div>
+                  <div class="grid-item"><strong>Board Size</strong><p>${panel.BoardSize}</p></div>
+                  <div class="grid-item"><strong>Lat/Long</strong><p><a href="https://www.google.com/maps?q=${panel.Lat},${panel.Long}" target="_blank">${panel.Lat}, ${panel.Long}</a></p></div>
+                  <div class="grid-item"><strong>Pricing</strong><p>${panel.Pricing}</p></div>
+                  <div class="grid-item"><strong>Illuminated</strong><p>${panel.Illuminated}</p></div>
+                </div>
+              </div>
+            `;
+            // Display contentString in your popup
+            showPopup(contentString, panel.Lat, panel.Long);
+            panelFound = true; // Panel was found
+            break; // Exit the loop once the panel is found
+          }
+        }
+        if (panelFound) break; // Exit outer loop if panel is found
+      }
+    }
+
+    if (!panelFound) {
+      console.error(`Panel with ID ${panelId} not found.`);
+    }
+  }
 
   function createKey(name, location) {
     var mapKey = document.getElementById("mapKeyPopup");
@@ -329,33 +445,26 @@ async function initMap() {
       console.error("map-key element not found.");
     }
   }
-  /////////////////////////////////////////////////////////////////////////////////////////
-}
 
-function openKeyPopup(markerId) {
-  console.log(markers);
-  var marker = markers.find(function (m) {
-    return m.ID === String(markerId);
-  });
-  if (marker && !popupOpen) {
-    popupOpen = true;
-    var contentString = `
+  function openKeyPopup(markerId) {
+    console.log(markers);
+    var marker = markers.find(function (m) {
+      return m.ID === String(markerId);
+    });
+    if (marker && !popupOpen) {
+      popupOpen = true;
+      var contentString = `
                             <button class="close-button" onclick="closePopup()">X</button>
                             <div class="popup-content">
-                                <h1>${marker.Location}</h1>
+                                <h1>${marker.Loc}</h1>
                                 <div class = "images-container">
                                     <div class = "image-item">
                                     <img src="Data/${
-                                      marker.ImageFront
+                                      marker.Img
                                     }" alt="Billboard Image Front">
-                                    <p><strong>${marker.FacingFront} Face</p>
+                                    <p><strong>${marker.Facing} Face</p>
                                     </div>
-                                    <div class = "image-item">
-                                    <img src="Data/${
-                                      marker.ImageBack
-                                    }" alt="Billboard Image Back">
-                                    <p><strong>${marker.FacingBack} Face</p>
-                                    </div>
+                                    <div id="miniMap"></div>
                                 </div>
                                 <div class="description-box">
                                     <p>${marker.Description}</p>
@@ -378,8 +487,8 @@ function openKeyPopup(markerId) {
                                         <p> <a href="https://www.google.com/maps?q=${
                                           marker.Lat
                                         },${marker.Long}" target="_blank">${
-      marker.Lat + ", " + marker.Long
-    }</a></p>
+        marker.Lat + ", " + marker.Long
+      }</a></p>
                                     </div>
                                     <div class="grid-item">
                                         <p class="title"><strong>Pricing</strong></p>
@@ -395,29 +504,41 @@ function openKeyPopup(markerId) {
                             </div>
                     `;
 
-    var overlay = document.createElement("div");
-    overlay.classList.add("overlay");
-    document.body.appendChild(overlay);
+      var overlay = document.createElement("div");
+      overlay.classList.add("overlay");
+      document.body.appendChild(overlay);
 
-    var popupDiv = document.createElement("div");
-    popupDiv.classList.add("custom-popup");
-    popupDiv.innerHTML = contentString;
-    map.getDiv().appendChild(popupDiv);
-    // Create and show the popup
-    // Position the popup at the center of the map
-    var popupOffsetX = popupDiv.offsetWidth / 2;
-    var popupOffsetY = popupDiv.offsetHeight / 2;
-    var center = map.getCenter();
-    var centerPixel = map.getProjection().fromLatLngToPoint(center);
-    var newPixelX = centerPixel.x - popupOffsetX;
-    var newPixelY = centerPixel.y - popupOffsetY;
-    var newLatLng = map
-      .getProjection()
-      .fromPointToLatLng(new google.maps.Point(newPixelX, newPixelY));
-    // Set the position of the popup
-    popupDiv.style.left = map.getDiv().offsetWidth / 3 + "px";
-    popupDiv.style.top = map.getDiv().offsetHeight / 9 + "px";
+      var popupDiv = document.createElement("div");
+      popupDiv.classList.add("custom-popup");
+      popupDiv.innerHTML = contentString;
+      map.getDiv().appendChild(popupDiv);
+      // Create and show the popup
+      // Position the popup at the center of the map
+      var popupOffsetX = popupDiv.offsetWidth / 2;
+      var popupOffsetY = popupDiv.offsetHeight / 2;
+      var center = map.getCenter();
+      var centerPixel = map.getProjection().fromLatLngToPoint(center);
+      var newPixelX = centerPixel.x - popupOffsetX;
+      var newPixelY = centerPixel.y - popupOffsetY;
+      var newLatLng = map
+        .getProjection()
+        .fromPointToLatLng(new google.maps.Point(newPixelX, newPixelY));
+      // Set the position of the popup
+      popupDiv.style.left = map.getDiv().offsetWidth / 3 + "px";
+      popupDiv.style.top = map.getDiv().offsetHeight / 9 + "px";
+
+      var miniMap = new google.maps.Map(document.getElementById("miniMap"), {
+        center: { lat: parseFloat(marker.Lat), lng: parseFloat(marker.Long) },
+        zoom: 15,
+        disableDefaultUI: true,
+      });
+
+      new google.maps.Marker({
+        position: { lat: parseFloat(marker.Lat), lng: parseFloat(marker.Long) },
+        map: miniMap,
+      });
+      console.log("Mini map should be visible now."); // Debugging line
+    }
   }
 }
-
 window.onload = initMap;
